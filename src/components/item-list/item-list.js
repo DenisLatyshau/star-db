068d5eaa4c697cdaec0136a324aa820ae1d74306
DenisLatyshau <denislatyshau@yandex.ru>
2023-03-04 21:46:36 +0300

    feat: finish item-list

--- src/components/item-list/item-list.js
+++ src/components/item-list/item-list.js
@@ -11,6 +11,18 @@ export default class ItemList extends Component {
     peopleList: null
   }
 
+  renderItems(arr) {
+    return arr.map(({id, name}) => {
+      return (
+        <li className='list-group-item'
+            key={id}
+            onClick={() => this.props.OnItemSelected(id)}>
+          {name}
+        </li>
+      )
+    })
+  }
+
   componentDidMount() {
     this.swapiService
         .getAllPeople()
@@ -29,17 +41,11 @@ export default class ItemList extends Component {
       return <Spinner />
     }
 
+    const items = this.renderItems(peopleList)
+
     return (
       <ul className="item-list list-group">
-        <li className="list-group-item">
-          Luke Skywalker
-        </li>
-        <li className="list-group-item">
-          Darth Vader
-        </li>
-        <li className="list-group-item">
-          R2-D2
-        </li>
+        {items}
       </ul>
     );
   }
